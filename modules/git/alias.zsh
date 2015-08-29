38e65a7655ca048e8efe011b7a6ce99a07e9e85a
Tim Schindler <tim@giantswarm.io>
2015-08-29 20:52:47 +0000

    fixed 'fatal: branch name required' for rmmerged

--- modules/git/alias.zsh
+++ modules/git/alias.zsh
@@ -27,7 +27,7 @@ zstyle -s ':prezto:module:git:status:ignore' submodules '_git_status_ignore_subm
 #
 
 # Custom
-alias rmmerged='git branch --merged master | grep -v "master$" | xargs git branch -d'
+alias rmmerged='git branch --merged master | grep -v "master$" | xargs -r git branch -d'
 alias ga='git add'
 alias gb='git branch'
 alias gc='git checkout'
